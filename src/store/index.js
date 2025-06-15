1111f0f8133963f4f456e02abc4a8551bdf76462
debanjan <sinha.debanjan708@gmail.com>
2025-06-15 12:51:51 +0530

    deletion of a commented file

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-// import postReducer from "../slice/postslice"
 import cardTaskReducer from "../slice/cardTaskReducerSlice"
 
 export const store = configureStore({
